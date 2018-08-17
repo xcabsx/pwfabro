01af477f6709d3402c3cd95981061bf4e61f41d4
cbereciartua <xcabsx@gmail.com>
2018-08-17 14:15:10 -0300

    agregando service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { MatCardModule, MatToolbarModule, MatButtonModule} from '@angular/materi
 import { AppComponent } from './app.component';
 import {ImgCardComponent} from './img-card/img-card.component';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +18,8 @@ import {ImgCardComponent} from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
 
   ],
   providers: [],
