480955c5332a4698eab8f4d145df69c2b00836ca
cbereciartua <xcabsx@gmail.com>
2018-08-17 16:32:17 -0300

    agregando service workers fafa

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -5,6 +5,10 @@ class CatImageComponent {
   api: string;
   fontsize: number;
 }
+class PepaComponent {
+  locat: string;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-img-card',
@@ -20,20 +24,28 @@ export class ImgCardComponent implements OnInit {
     api: 'https://cataas.com/cat/says/',
     fontsize: 40
   };
-
+  public images: string[];
   public src: string;
 
-  constructor() { }
+  constructor() {
+    this.images = ['assets/d1.jpg', 'assets/d2.jpg', 'assets/d3.jpg', 'assets/d4.png'];
+  }
+
 
   ngOnInit() {
      this.src = this.image.api + this.image.message;
      this.generateSrc();
+
   }
 
   generateSrc(): void {
-    this.src = this.image.api + this.image.message +
+
+    let show = this.images[Math.floor(Math.random() * this.images.length)];
+
+    this.src = show;
+    /*this.src = this.image.api + this.image.message +
       '?size=' + this.image.fontsize +
-      '&ts=' + Date.now();
+      '&ts=' + Date.now();*/
   }
 
 }
